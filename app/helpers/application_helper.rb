b5a042492c01af75088af83c00d88a6e5e6f41eb
jjhyh <jjhyh@example.com>
2014-11-12 15:02:51 -0800

    Add week2 features

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def fix_url(url)
+    if url.start_with?('http://') || url.start_with?('https://')
+      return url
+    else
+      return "http://#{url}"
+    end
+  end
+
+  def display_date(dt)
+    dt.strftime('%m/%d/%Y %l:%M %p %Z')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
   resources :posts, except: [:destroy] do
-  	resources :comments, only: [:create]
+    resources :comments, only: [:create]
   end
+  resources :categories, only: [:new, :create, :show]
 end
